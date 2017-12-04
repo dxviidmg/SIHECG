1d57942fb28ffcc6d029ad8de9d85c9b2ee210b3
dxviidmg <dmg_92@hotmail.com>
2017-12-04 16:54:58 -0600

    v1.1.1

--- SIHECG/settings.py
+++ SIHECG/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '=68+(wx&q%z(jlse(7e(l%x-s=j_44bdovpl5x#7m9_^mgafxj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
