8201ef3d613ac46d95de4b71098d165e88527d79
dxviidmg <dmg_92@hotmail.com>
2017-12-04 20:58:30 -0600

    v1.1.2 verbose names

--- accounts/models.py
+++ accounts/models.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 	user = models.OneToOneField(User, related_name="user", null=True, blank=True)
-	phone = models.CharField(max_length=12)
-	age = models.IntegerField()
+	phone = models.CharField(max_length=12, verbose_name="Teléfono")
+	age = models.IntegerField(verbose_name="Edad")
 	doctor = models.ForeignKey(User, related_name="doctor", null=True, blank=True)
 
 	def __str__(self):
